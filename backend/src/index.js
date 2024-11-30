a465c8186947b2f3abd3023b650a3da675ff5ce4
Shaikh-Abdullah <shabdullah685@gmail.com>
2024-11-30 19:29:31 +0530

    created signup API and test on postmen

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,19 @@
 import express from "express"
-
+import dotenv from "dotenv"
+import { connectDB } from './lib/db.js'
 import authRouths from "./routes/auth.route.js"
 
+dotenv.config()
 const app = express()
+
+const PORT = process.env.PORT
+
+app.use(express.json())
 
 app.use("/api/auth", authRouths)
 
 
-app.listen(5001, () => {
-    console.log("Server in running on port 5001");
+app.listen(PORT, () => {
+    console.log(`Server in running on port ${PORT}`);
+    connectDB()
 })
