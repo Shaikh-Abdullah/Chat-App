fa967593614bcba9a16ebf008419f4ac3abbf3df
Shaikh-Abdullah <shabdullah685@gmail.com>
2025-03-15 09:03:06 +0530

    added new API end point tested on Postman

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieparser from "cookie-parser"
+
 import { connectDB } from './lib/db.js'
 import authRouths from "./routes/auth.route.js"
 
@@ -9,6 +11,7 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cookieparser())
 
 app.use("/api/auth", authRouths)
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,12 @@
 import express from "express"
 import { signup, login, logout } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup)
-
 router.post("/login", login)
-
 router.post("/logout", logout)
+router.put("/update-profile", protectRoute, updateProfile) // PUT method is basically use for update something
 
 export default router
