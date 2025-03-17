61f73c8c5ca295e7cca7ad629c15938926efdb67
Shaikh-Abdullah <shabdullah685@gmail.com>
2025-03-17 20:01:28 +0530

    fixed cors error and added zustand state management

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieparser from "cookie-parser"
+import cors from "cors"
 
 import { connectDB } from './lib/db.js'
 import authRouths from "./routes/auth.route.js"
@@ -13,6 +14,10 @@ const PORT = process.env.PORT
 
 app.use(express.json())
 app.use(cookieparser())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRouths)
 app.use("/api/message", messageRouths)
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./component/Navbar";
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage"
-import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import Settingspage from "./pages/Settingspage";
 import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 
 const App = () => {
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+
   return (
     <div>
       <Navbar />
