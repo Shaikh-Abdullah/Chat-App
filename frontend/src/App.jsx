3899ba754c03c1cbf5ba627a6267676064b18f92
Shaikh-Abdullah <shabdullah685@gmail.com>
2025-03-18 18:23:33 +0530

    adding procted route for homepage and setting page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import Navbar from "./component/Navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -28,10 +28,10 @@ const App = () => {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/settings" element={<Settingspage />} />
+        <Route path="/settings" element={auth ? <Settingspage /> : <Navigate to="/login" />} />
         <Route path="/profile" element={<ProfilePage />} />
       </Routes>
     </div>
