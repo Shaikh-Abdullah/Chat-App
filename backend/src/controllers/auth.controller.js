dc3917b8d625911a85ffffc82c5cc2006fc9c948
Shaikh-Abdullah <shabdullah685@gmail.com>
2025-03-23 12:13:42 +0530

    created Profile page UI and Integrated it

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
             _id: user._id,
             fullName: user.fullName,
             email: user.email,
-            profilePic: user.profile,
+            profilePic: user.profilePic,
         })
 
     } catch (error) {
--- backend/src/index.js
+++ backend/src/index.js
@@ -9,10 +9,10 @@ import messageRouths from "./routes/message.route.js"
 
 dotenv.config()
 const app = express()
-
 const PORT = process.env.PORT
 
-app.use(express.json())
+app.use(express.json({ limit: "50mb" }))
+app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(cookieparser())
 app.use(cors({
     origin: "http://localhost:5173",
