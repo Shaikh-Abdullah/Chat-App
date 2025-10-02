c90f31c30323efe4c97fd4175a602b5353cc4107
Shaikh-Abdullah <shabdullah685@gmail.com>
2025-10-03 00:22:40 +0530

    home page ui conversion

---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,35 @@
+import { create } from "zustand";
+import toast from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set) => ({
+    message: [],
+    users: [],
+    selectedUser: null,
+    isUserLoading: false,
+    isMessagesLoading: false,
+
+    getUsers: async () => {
+        set({ isUserLoading: true });
+        try {
+            const res = await axiosInstance.get("/messages/users");
+            set({ users: res.data});
+        } catch (error) {
+            toast.error(error?.response?.data?.message || "Failed to fetch users");
+        } finally {
+            set({ isUserLoading: false });
+        }
+    },
+
+    getMessages: async (userId) => {
+        set({ isMessagesLoading: true });
+        try {
+            const res = await axiosInstance.get(`/messages/${userId}`);
+            set({ message: res.data });
+        } catch (error) {
+            toast.error(error?.response?.data?.message || "Failed to fetch messages");
+        } finally {
+            set({ isMessagesLoading: false });
+        }
+    }
+}))
